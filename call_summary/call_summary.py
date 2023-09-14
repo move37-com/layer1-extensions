c98db9b173ca3306d37c46f11cf34bcbb1ac27e5
Collin Hundley <collinhundley@gmail.com>
2023-09-13 18:18:15 -0700

    Module for layer1 connections + support pattern matching subscriptions

--- call_summary/call_summary.py
+++ call_summary/call_summary.py
@@ -1,73 +1,12 @@
-import redis.asyncio as redis
+import layer1
 import asyncio
 import json
 import uuid
 
-class MessageCenter:
-    queue = {}
-    r = redis.Redis(host='localhost', port=6381, decode_responses=True)
-
-    def __init__(self, loop, extension_id):
-        self.loop = loop
-        self.extension_id = extension_id
-
-    def run(self):
-        pubsub = self.r.pubsub()
-        self.loop.run_until_complete(self.listen(pubsub))
-    
-    async def send_message(self, msg):
-        responseID = str(uuid.uuid4())
-        msg["responseID"] = responseID
-        msg["extensionID"] = self.extension_id
-        msg["origin"] = "extension"
-
-        resp_future = asyncio.Future()
-        self.queue[responseID] = resp_future
-        await self.r.publish("messages", json.dumps(msg))
-        result = await resp_future
-        return result
-
-    async def listen(self, pubsub):
-        await pubsub.subscribe('events', 'messages')
-        while True:
-            try:
-                msg = await pubsub.get_message(ignore_subscribe_messages=True, timeout=1.0)
-                if msg:
-                    self.triage_msg(msg['channel'], json.loads(msg['data']))
-            except asyncio.CancelledError:
-                raise
-            except:
-                print("Connection closed")
-                await asyncio.sleep(1)
-
-    def triage_msg(self, channel, msg):
-        match channel:
-            case 'events':
-                if self.event_handler:
-                    try:
-                        self.loop.create_task(self.event_handler(msg['event'], msg['data']))
-                    except Exception as e:
-                        print("Event handler error raised: ", e)
-            case 'messages':
-                # Only handle incoming messages directed at our extension
-                # Also ignore any messages broadcast by ourself (origin)
-                if msg['origin'] == 'app' and msg['extensionID'] == self.extension_id:
-                    self.handle_response(msg['responseID'], msg['data'])
-        
-    # Handles incoming responses on the 'messages' channel
-    def handle_response(self, responseID, msg):
-        if self.queue[responseID]:
-            self.queue[responseID].set_result(msg)
-            del self.queue[responseID]
-        else:
-            print("Warning: no response handler found for responseID: ", responseID)
-
-
-
 # Create a MessageCenter instance
 extension_id = str(uuid.uuid4())
 loop = asyncio.get_event_loop()
-message_center = MessageCenter(loop, extension_id)
+message_center = layer1.MessageCenter(loop, extension_id)
 
 # 
 async def handleCallDidEnd(msg):
@@ -106,13 +45,11 @@ async def handleCallDidEnd(msg):
     print("Render status: ", status)
 
 # Handler for incoming global events
-async def event_handler(event, msg):
-    print("Got global event: ", event)
+async def event_handler(channel, event, msg):
     match event:
         case 'callRecordings.callDidEnd':
             await handleCallDidEnd(msg)
 
-
 # Register event handler and start the message center
-message_center.event_handler = event_handler
-message_center.run()
+message_center.subscribe('events', event_handler)
+message_center.run() # Will run forever
